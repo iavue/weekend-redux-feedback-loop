8833b89dc092c5ca6cbc810b981d4f5d02840472
Ia Vue <iavue00@gmail.com>
2023-03-30 21:05:44 -0500

    set up redux in index.js, started planning in app.jsx, and set up database in postico.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+
+// Import components here
+
 function App() {
 
   return (
@@ -10,6 +13,10 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      {/* One component goes here which will be the 'feeling' component.
+      Inside the 'feeling' component, will have a form with an input and a 'next' button. 
+      Then there should be a route to the next component with the same stuff, and the next
+      component with the same stuff, and so on... these will all be nested components. */}
     </div>
   );
 }
